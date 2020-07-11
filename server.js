804121f22b89aed314dcf4763508a1a7f849ef27
Benny Samir Hierl <bennysamir@posteo.de>
2020-07-11 21:26:08 +0200

    fix
    Signed-off-by: Benny Samir Hierl <bennysamir@posteo.de>

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ if (!process.env.apikey) throw new Error('ENV apikey is missing!');
 if (!process.env.traefik_network) throw new Error('ENV traefik_network is missing!');
 
 const defaultLabels = {
-  'traefik.enable': true,
+  'traefik.enable': 'true',
 };
 
 /*
@@ -76,7 +76,7 @@ const startReviewApp = async (req, res) => {
   };
   Labels[`traefik.http.routers.${body.host.replace('.', '-')}.rule`] = `Host(\`${body.host}\`)`;
   if (process.env.traefik_certresolver) {
-    Labels[`traefik.http.routers.${body.host.replace('.', '-')}.tls`] = true;
+    Labels[`traefik.http.routers.${body.host.replace('.', '-')}.tls`] = 'true';
     Labels[`traefik.http.routers.${body.host.replace('.', '-')}.tls.certresolver`] = process.env.traefik_certresolver;
   }
 
