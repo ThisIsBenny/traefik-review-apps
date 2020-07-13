02cf7812efdf540949ffca09eb34c7f6aaa83a93
Benny Samir Hierl <bennysamir@posteo.de>
2020-07-13 11:14:39 +0200

    fix linter issue
    Signed-off-by: Benny Samir Hierl <bennysamir@posteo.de>

--- plugins/dummy.plugin.js
+++ plugins/dummy.plugin.js
@@ -1,15 +1,18 @@
 const failure = (error, { hostname, image }) => {
+  global.logger.info('Execute dummy plugin (failure)');
   global.logger.debug(`Dummy Plugin: Error => ${error}`);
+  global.logger.debug(`Dummy-Plugin - Hostname: ${hostname}`);
+  global.logger.debug(`Dummy-Plugin - Image: ${image}`);
 };
 const predeployment = ({ hostname, image }) => {
   global.logger.info('Execute dummy plugin (predeployment)');
-  global.logger.debug(`Dummy-Pluign - Hostname: ${hostname}`);
-  global.logger.debug(`Dummy-Pluign - Image: ${image}`);
+  global.logger.debug(`Dummy-Plugin - Hostname: ${hostname}`);
+  global.logger.debug(`Dummy-Plugin - Image: ${image}`);
 };
 const postdeployment = ({ hostname, image }) => {
   global.logger.info('Execute dummy plugin (postdeployment)');
-  global.logger.debug(`Dummy-Pluign - Hostname: ${hostname}`);
-  global.logger.debug(`Dummy-Pluign - Image: ${image}`);
+  global.logger.debug(`Dummy-Plugin - Hostname: ${hostname}`);
+  global.logger.debug(`Dummy-Plugin - Image: ${image}`);
 };
 
 module.exports = {
