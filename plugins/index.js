f0a0a6782212caf792048e32f31836cb808a86dd
Benny Samir Hierl <bennysamir@posteo.de>
2020-07-13 11:39:54 +0200

    fix issue with not defined plugins env
    Signed-off-by: Benny Samir Hierl <bennysamir@posteo.de>

--- plugins/index.js
+++ plugins/index.js
@@ -4,31 +4,33 @@ const postdeploymentPlugins = [];
 
 const bootstrap = () => {
   global.logger.info('Plugins will be registered');
-  process.env.plugins.split(',').forEach((e) => {
-    try {
-      global.logger.info(`Register ${e} plugin`);
-      const plugin = require(`./${String(e)}.plugin`); // eslint-disable-line security/detect-non-literal-require, global-require, import/no-dynamic-require
+  if (process.env.plugins) {
+    process.env.plugins.split(',').forEach((e) => {
+      try {
+        global.logger.info(`Register ${e} plugin`);
+        const plugin = require(`./${String(e)}.plugin`); // eslint-disable-line security/detect-non-literal-require, global-require, import/no-dynamic-require
 
-      plugin.requiredEnvs.forEach((env) => {
-        if (!process.env[String(env)]) throw new Error(`Required ENV ${env} for plugin ${e} is missing!`);
-      });
+        plugin.requiredEnvs.forEach((env) => {
+          if (!process.env[String(env)]) throw new Error(`Required ENV ${env} for plugin ${e} is missing!`);
+        });
 
-      if (typeof plugin.failure === 'function') {
-        global.logger.debug(`Register failure function for ${e.trim()} plugin`);
-        failurePlugins.push(plugin.failure);
+        if (typeof plugin.failure === 'function') {
+          global.logger.debug(`Register failure function for ${e.trim()} plugin`);
+          failurePlugins.push(plugin.failure);
+        }
+        if (typeof plugin.predeployment === 'function') {
+          global.logger.debug(`Register predeployment function for ${e.trim()} plugin`);
+          predeploymentPlugins.push(plugin.predeployment);
+        }
+        if (typeof plugin.postdeployment === 'function') {
+          global.logger.debug(`Register postdeployment function for ${e.trim()} plugin`);
+          postdeploymentPlugins.push(plugin.postdeployment);
+        }
+      } catch (error) {
+        global.logger.warn(`${e} Plugin can't be registered: ${error}`);
       }
-      if (typeof plugin.predeployment === 'function') {
-        global.logger.debug(`Register predeployment function for ${e.trim()} plugin`);
-        predeploymentPlugins.push(plugin.predeployment);
-      }
-      if (typeof plugin.postdeployment === 'function') {
-        global.logger.debug(`Register postdeployment function for ${e.trim()} plugin`);
-        postdeploymentPlugins.push(plugin.postdeployment);
-      }
-    } catch (error) {
-      global.logger.warn(`${e} Plugin can't be registered: ${error}`);
-    }
-  });
+    });
+  }
 };
 
 const failure = (error, reqBody) => {
